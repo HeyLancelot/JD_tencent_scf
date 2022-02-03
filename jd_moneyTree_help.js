ff3fcec084d874a32238ea9095821c85eb89ee36
HeyLancelot <73233270+HeyLancelot@users.noreply.github.com>
2022-02-04 01:40:12 +0800

    Update jd_moneyTree_help.js

--- jd_moneyTree_help.js
+++ jd_moneyTree_help.js
@@ -261,8 +261,8 @@ function shareCodesFormat() {
   return new Promise(async resolve => {
     $.newShareCodes = [];
     let inviteCodes = [
-      't_7LVGP8mopofh8AG0Q7E8AdoUJQ3Dik@zExA7lNc3HrJrbVuG3xRVMAdoUJQ3Dik@cvwWiz9o2evNHFdNk0oNbMAdoUJQ3Dik@8MQ6wrd9H0IAujNGUqzTAA@0IzWPVQGlmepafqlqgOSXw',
-      't_7LVGP8mopofh8AG0Q7E8AdoUJQ3Dik@zExA7lNc3HrJrbVuG3xRVMAdoUJQ3Dik@cvwWiz9o2evNHFdNk0oNbMAdoUJQ3Dik@8MQ6wrd9H0IAujNGUqzTAA@0IzWPVQGlmepafqlqgOSXw'
+      'iX7lnfg3z3bb80agdSAYJw@NvGuBJi4PBXei-56xJnB_cAdoUJQ3Dik@bwogBSA8wfSjn7bXqRrZmw',
+      'iX7lnfg3z3bb80agdSAYJw@NvGuBJi4PBXei-56xJnB_cAdoUJQ3Dik@bwogBSA8wfSjn7bXqRrZmw'
     ];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
